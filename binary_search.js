c7563926ed56c4c156f8de5f866e01db8ea00173
MRaahulVignesh <41645355+MRaahulVignesh@users.noreply.github.com>
2022-10-04 21:02:04 -0700

    added a dummy variable

--- binary_search.js
+++ binary_search.js
@@ -1,7 +1,7 @@
 function binarySearch(sortedArray, key){
     let start = 0;
     let end = sortedArray.length - 1;
-
+    let result = -1;
     while (start <= end) {
         let middle = Math.floor((start + end) / 2);
 
